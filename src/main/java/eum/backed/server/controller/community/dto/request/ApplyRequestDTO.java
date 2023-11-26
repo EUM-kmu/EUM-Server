8a66a639ad3943d646eec9011b9c577c655801d0
solhee-hwang <solhee0912@kookmin.ac.kr>
2023-11-26 14:42:24 +0900

    fix : signin with kakao

--- src/main/java/eum/backed/server/controller/community/dto/request/ApplyRequestDTO.java
+++ src/main/java/eum/backed/server/controller/community/dto/request/ApplyRequestDTO.java
@@ -1,24 +1,21 @@
 package eum.backed.server.controller.community.dto.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
-import java.util.List;
 
 public class ApplyRequestDTO {
     @Getter
     @Setter
     public static class Apply{
+        @ApiModelProperty(example = "1",value = "게시글 아이디")
         @NotEmpty
         private Long postId;
+        @ApiModelProperty(example = "저는 요리를 잘합니다",value = "지원 한마디")
         private String introduction;
-    }
 
-    @Getter
-    @Setter
-    public static class AcceptList{
-        private List<Long> applyIds;
     }
 
 
